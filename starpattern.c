488b7cb8dbc8400dcee77378b33c2296a7414522
prasanna1432 <71954483+prasanna1432@users.noreply.github.com>
2020-10-14 09:35:58 +0530

    Create starpattern.c

---
+++ starpattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  
+int main()  
+{  
+    int n=9;  
+    for(int i=1;i<=n;i++)  
+    {  
+        for(int j=1;j<=n;j++)  
+        {  
+            if(i==1 ||i==n||j==1||j==n-i+1||i==j||j==n)  
+            {  
+            printf("*");  
+            }  
+            else  
+            {  
+                  
+                      printf(" ");  
+                  }  
+                 
+            }        
+        printf("\n");  
+    }  
+      
+    return 0;  
+} 
